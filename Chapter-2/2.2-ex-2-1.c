2e089509f8109c5591a2c024e3ffcde1d8669c44
sakib <sakibul706@gmail.com>
2022-07-12 05:43:08 +0600

    C2

---
+++ Chapter-2/2.2-ex-2-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+
+void main()
+{
+	printf("max sc %d\n", SCHAR_MAX);
+	printf("mim sc %d\n", SCHAR_MIN);
+	printf("max uc %d\n", UCHAR_MAX);
+	printf("mim uc %d\n", UCHAR_MIN);
+}
